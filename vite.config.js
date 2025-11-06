46a5755ec3910a6c7887ba13d14c9af0b3009591
cbins13 <christianbinayan3@gmail.com>
2025-11-06 11:53:14 +0800

    message

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/shoppingreact",
+  base: "https://cbins13.github.io/e-plantShopping/",
   plugins: [react()],
   esbuild: {
     loader: 'jsx'
