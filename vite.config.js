71512eb6fbf00fcfc97234cc9ccefcf513c12f73
cbins13 <christianbinayan3@gmail.com>
2025-11-06 11:50:37 +0800

    message

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,21 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/shoppingreact/",
+  base: "/shoppingreact",
   plugins: [react()],
+  esbuild: {
+    loader: 'jsx'
+  },
+  resolve: {
+    alias: {
+      './runtimeConfig': './runtimeConfig.browser'
+    }
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      loader: {
+        '.js': 'jsx'
+      }
+    }
+  }
 })
